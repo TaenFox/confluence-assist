94cc099c17075a3b535598ecf90228559c751c28
pmokeev <pmokeev@moysklad.ru>
2024-03-27 16:38:19 +0300

    Фрагмент для добавления новых меток по старым

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from atlassian import Confluence
-import os
+import os, json
 
 # перед началом работы создать виртуальную среду командой `python3 -m venv venv`
 # записать в конец файла venv/bin/activate строки bash-скрипта для экспорта
@@ -17,4 +17,20 @@ confluence = Confluence(
     username=LOGIN,     # user@domen.com
     password=TOKEN,     # токен, сгенерированный на странице https://id.atlassian.com/manage-profile/security/api-tokens
     cloud=True)
-print(confluence.get_all_spaces(start=0, limit=500, expand=None))
+
+'''
+# Фрагмент кода, который добавляет новую метку label_B ко всем страницам, которые содержат метку label_A
+all_content = confluence.get_all_pages_by_label("label_A", start=0, limit=500)
+i=0
+# Выводим список страниц
+for page in all_content:
+    try:
+        i+=1
+        print(f"Страница ID: {page['id']} - {page['title']}")
+        result = confluence.set_page_label(page['id'], "label_B")
+        for piece in result['results']:
+            print(piece)
+    except Exception as e:
+        print(f"Ошибка при установке метки на странице {page['id']}: {str(e)}")
+print(f"Старниц обработано {i}")
+'''
