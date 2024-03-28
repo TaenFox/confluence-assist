ccb8586356953dadf1e6f437ddf69568ae63e0bd
pmokeev <pmokeev@moysklad.ru>
2024-03-28 10:04:52 +0300

    Добавил фрагмент кода для удаления меток

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from atlassian import Confluence
 import os, json
 
+#   документация на библиотеку https://atlassian-python-api.readthedocs.io/confluence.html
 # перед началом работы создать виртуальную среду командой `python3 -m venv venv`
 # записать в конец файла venv/bin/activate строки bash-скрипта для экспорта
 # указанных ниже переменных (пример строки): export CONFLUENCE_LOGIN="user@domen.com" 
@@ -20,6 +21,7 @@ confluence = Confluence(
 
 '''
 # Фрагмент кода, который добавляет новую метку label_B ко всем страницам, которые содержат метку label_A
+
 all_content = confluence.get_all_pages_by_label("label_A", start=0, limit=500)
 i=0
 # Выводим список страниц
@@ -32,5 +34,19 @@ for page in all_content:
             print(piece)
     except Exception as e:
         print(f"Ошибка при установке метки на странице {page['id']}: {str(e)}")
+print(f"Старниц обработано {i}")
+'''
+'''
+# Фрагмент кода, который удаляет метку label_A на страницах, которые содержат метку label_B
+
+all_content = confluence.get_all_pages_by_label("label_B", start=0, limit=500)
+i=0
+for page in all_content:
+    try:
+        i+=1
+        print(f"Страница ID: {page['id']} - {page['title']}")
+        confluence.remove_page_label(page['id'], "label_A")
+    except Exception as e:
+        print(f"Ошибка при удалении метки на странице {page['id']}: {str(e)}")
 print(f"Старниц обработано {i}")
 '''
