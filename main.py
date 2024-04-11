2b58d219748a815da766f7cbe8c1e81a2a3d3250
pmokeev <pmokeev@moysklad.ru>
2024-04-11 14:41:54 +0300

    Выгрузка страниц

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ TOKEN = os.environ.get("ATLASSIAN_TOCKEN")
 URL = os.environ.get("CONFLUENCE_URL")
 
 confluence = Confluence(
-    url=URL,            # адрес, указанный со схемой: "http://xxxxxxx.atlassian.com/wiki"
+    url=URL,            # адрес, указанный со схемой: "https://xxxxxxx.atlassian.com/wiki"
     username=LOGIN,     # user@domen.com
     password=TOKEN,     # токен, сгенерированный на странице https://id.atlassian.com/manage-profile/security/api-tokens
     cloud=True)
@@ -49,4 +49,36 @@ for page in all_content:
     except Exception as e:
         print(f"Ошибка при удалении метки на странице {page['id']}: {str(e)}")
 print(f"Старниц обработано {i}")
+'''
+
+'''
+# Фрагмент кода, который экспортирует содержимое страницы и её подстраниц
+# ID страницы Confluence, которую вы хотите экспортировать
+page_id = "1249116312"
+
+def export_page_to_md(page_id, parent_dir):
+    page = confluence.get_page_by_id(page_id, expand='body.storage,title')
+    page_title = page['title'].replace('/', '_')  # Замена / в названии, чтобы избежать проблем с путями файловой системы
+
+    # Создание подкаталога для страницы
+    current_dir = os.path.join(parent_dir, get_code_word(page_title))
+    os.makedirs(current_dir, exist_ok=True)
+
+    # Создание и запись в файл
+    filename = os.path.join(current_dir, f"{page_title}.md")
+    with open(filename, 'w', encoding='utf-8') as file:
+        file.write(page['body']['storage']['value'])
+        print(f"Содержимое страницы {page_title} экспортировано в {filename}")
+
+    # Рекурсивный экспорт дочерних страниц
+    child_pages = confluence.get_child_pages(page_id)
+    for child in child_pages:
+        export_page_to_md(child['id'], current_dir)
+
+def get_code_word(text):
+    # Разбиение строки по пробелу и возврат первого элемента
+    return text.split(' ')[0]
+
+root_dir = '.'  # Корневая директория для экспорта
+export_page_to_md(page_id, root_dir)
 '''
